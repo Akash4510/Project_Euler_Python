0d67c1039718feab61b5efd5250a2295e31a3d3f
Akash Gupta <78362903+Akash4510@users.noreply.github.com>
2021-04-13 14:25:00 +0530

    Added comment

--- Q1.  Multiples of 3 and 5.py
+++ Q1.  Multiples of 3 and 5.py
@@ -1,5 +1,5 @@
 my_list = []
-for x in range(0, 1000):
+for x in range(0, 1000):  # Since up to 1000 is mentioned in the problem.
     if x % 3 == 0 or x % 5 == 0:
         my_list.append(x)
 print(sum(my_list))
