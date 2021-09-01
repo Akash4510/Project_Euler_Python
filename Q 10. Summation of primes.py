19e0cbca3272a4754830470d11c0800d1343f99c
Akash Gupta <78362903+Akash4510@users.noreply.github.com>
2021-09-01 19:17:16 +0530

    Solution for summation of primes.

---
+++ Q 10. Summation of primes.py
@@ -0,0 +1,24 @@
+from math import isqrt
+
+
+def primes_less_than(n: int) -> list[int]:
+    if n <= 2:
+        return []
+    is_prime = [True] * n
+    is_prime[0] = False
+    is_prime[1] = False
+
+    for i in range(2, isqrt(n)):
+        if is_prime:
+            for x in range(i*i, n, i):
+                is_prime[x] = False
+
+    return [num for num in range(n) if is_prime[num]]
+
+
+if __name__ == '__main__':
+    number = 2000
+    primes_list = primes_less_than(number)
+    total = sum(primes_list)
+    print(f"There are {len(primes_list)} prime numbers below {number} which are listed below:\n{primes_list}")
+    print(f"The Sum of all the prime numbers below {number} is: {total}")
